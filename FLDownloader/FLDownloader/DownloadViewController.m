63d87bb2ee10360761d3dc1ee233c7454f5a23cd
zhangwenhua <zhangwenhua@cheyaoshi.com>
2016-08-02 10:51:22 +0800

    code test

--- FLDownloader/FLDownloader/DownloadViewController.m
+++ FLDownloader/FLDownloader/DownloadViewController.m
@@ -17,14 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    if (true) NSLog(@"AAAAAA");
-    
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+  NSLog(@"AAAAAAA");
+    if (true) NSLog(@"BBB");
 }
 
 - (void)didReceiveMemoryWarning {
