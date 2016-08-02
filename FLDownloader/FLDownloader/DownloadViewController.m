d4204d39986b7327c787bccc5e65177e47d448b9
zhangwenhua <zhangwenhua@cheyaoshi.com>
2016-08-02 10:58:21 +0800

    test

--- FLDownloader/FLDownloader/DownloadViewController.m
+++ FLDownloader/FLDownloader/DownloadViewController.m
@@ -8,32 +8,38 @@
 
 #import "DownloadViewController.h"
 
+
 @interface DownloadViewController ()
 
 @end
 
+
 @implementation DownloadViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    
-  NSLog(@"AAAAAAA");
+
+    NSLog(@"AAAAAAA");
     if (true) NSLog(@"BBB");
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
 #warning Incomplete implementation, return the number of sections
     return 0;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
 #warning Incomplete implementation, return the number of rows
     return 0;
 }
